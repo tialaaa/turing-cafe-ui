e1e08e09b5646d0e1e4901270114c0c1fb4bb4d0
Tiala Young <tialayoung@gmail.com>
2023-05-30 11:30:00 -0600

    Write controlled form render

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -8,15 +8,42 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      guests: 0,
+      number: '',
     }
-  }
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  };
 
   render() {
     return (
-      <div></div>
+      <form>
+        <input type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)} required/>
+        <input type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)} required/>
+        <input type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)} required/>
+        <input type='number'
+          placeholder='Number of guests'
+          name='number'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)} required/>
+
+        <button className='button-make-resy'>Make Reservation</button>
+      </form>
     )
-  }
+  };
 }
 
 export default Form;
