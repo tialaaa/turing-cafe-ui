2d258977e932ba8634e2837e8fc8325eabea8e2a
Tiala Young <tialayoung@gmail.com>
2023-05-30 10:30:06 -0600

    Add props and JSX return statement

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,12 +1,19 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import './Reservation.css';
 
-function Reservation() {
-
-
-  // return (
-
-  // )
+function Reservation({ name, date, time, number }) {
+  return (
+    <section>
+      <h3>${name}</h3>
+      <p>${date}</p>
+      <p>${time} pm</p>
+      <p>Number of guests: ${number}</p>
+      <button>Cancel</button>
+    </section>
+  );
 }
+
+// add Proptypes
 
 export default Reservation;
