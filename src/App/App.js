b92d2b2ca6c476e4324b7e365f13cc9bf5b92a1f
Tiala Young <tialayoung@gmail.com>
2023-05-30 09:36:35 -0600

    Setup all components

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form';
+import Container from '../Res_Container/Container';
+import Reservation from '../Reservation/Reservation';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/Reservation/Reservation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reservation.css';
+
+function Reservation() {
+
+
+  // return (
+
+  // )
+}
+
+export default Reservation;
