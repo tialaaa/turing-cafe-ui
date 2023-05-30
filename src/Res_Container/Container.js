af0a6b81fce958002833f4d6109e91e9dd20dcd8
Tiala Young <tialayoung@gmail.com>
2023-05-30 10:20:17 -0600

    Write Container's array mapping and return statement

--- src/Res_Container/Container.js
+++ src/Res_Container/Container.js
@@ -1,12 +1,30 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import './Container.css';
+import Reservation from '../Reservation/Reservation';
 
-function Container() {
+function Container(reservations) {
+  const resyCards = reservations.map(resy => {
+    return (
+      <Reservation 
+        key={resy.id}
+        id={resy.id}
+        name={resy.name}
+        date={resy.date}
+        time={resy.time}
+        number={resy.number}
+      />
+    )
+  });
 
-
-  // return (
-
-  // )
+  return (
+    <section className='resy-container'>
+      { resyCards }
+    </section>
+  );
 }
+
+// install PropTypes dependency
+// & add Proptypes
 
 export default Container;
